981ca23f4f6c79b06ce969a71d8eb1e6020f1c07
pierryjohn01 <johnpierry18@gmail.com>
2025-03-19 13:50:11 -0300

    Add files via upload

---
+++ aulas DS 2025/atividade montar carro com combobox/montarcarro/montarcarro/Form1.cs
@@ -0,0 +1,50 @@
+namespace montarcarro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            comboBox1.Text = "";
+            comboBox2.Text = "";
+            comboBox3.Text = "";
+            comboBox4.Text = "";
+            label6.Text = "";
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label6.Text = "Descrição: ";
+            label7.Text = comboBox1.Text;
+            label8.Text = comboBox2.Text;
+            label9.Text = comboBox3.Text;
+            label9.Text = comboBox4.Text;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
