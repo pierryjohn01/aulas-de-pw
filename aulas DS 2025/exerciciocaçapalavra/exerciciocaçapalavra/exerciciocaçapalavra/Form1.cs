71b4148057370ac034d7f7f76bd32d225c5d7977
pierryjohn01 <johnpierry18@gmail.com>
2025-04-01 14:40:08 -0300

    Add files via upload

---
+++ aulas DS 2025/exerciciocaçapalavra/exerciciocaçapalavra/exerciciocaçapalavra/Form1.cs
@@ -0,0 +1,115 @@
+namespace exerciciocaçapalavra
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            label6.BackColor = Color.Yellow;
+            label7.BackColor = Color.Yellow;
+            label17.BackColor = Color.Yellow;
+            label22.BackColor = Color.Yellow;
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+            label12.BackColor = Color.DarkCyan;
+            label13.BackColor = Color.DarkCyan;
+            label18.BackColor = Color.DarkCyan;
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            label8.BackColor = Color.Gray;
+            label9.BackColor = Color.Gray;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            label2.BackColor = Color.Red;
+            label3.BackColor = Color.Red;
+            label17.BackColor = Color.Red;
+            label18.BackColor = Color.Red;
+            label19.BackColor = Color.Red;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.BackColor = TransparencyKey;
+            label3.BackColor = TransparencyKey;
+            label4.BackColor = TransparencyKey;
+            label5.BackColor = TransparencyKey;
+            label6.BackColor = TransparencyKey;
+            label7.BackColor = TransparencyKey;
+            label8.BackColor = TransparencyKey;
+            label9.BackColor = TransparencyKey;
+            label10.BackColor = TransparencyKey;
+            label11.BackColor = TransparencyKey;
+            label12.BackColor = TransparencyKey;
+            label13.BackColor = TransparencyKey;
+            label14.BackColor = TransparencyKey;
+            label15.BackColor = TransparencyKey;
+            label16.BackColor = TransparencyKey;
+            label17.BackColor = TransparencyKey;
+            label18.BackColor = TransparencyKey;
+            label19.BackColor = TransparencyKey;
+            label20.BackColor = TransparencyKey;
+            label22.BackColor = TransparencyKey;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            label4.BackColor = Color.Green;
+            label5.BackColor = Color.Green;
+            label19.BackColor = Color.Green;
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+            label10.BackColor = Color.IndianRed;
+            label11.BackColor = Color.IndianRed;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+            label16.BackColor = Color.PapayaWhip;
+            label20.BackColor = Color.PapayaWhip;
+        }
+
+        private void label20_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+            label14.BackColor = Color.WhiteSmoke;
+            label15.BackColor = Color.WhiteSmoke;
+        }
+
+        private void label57_Click(object sender, EventArgs e)
+        {
+            label57.BackColor = Color.DarkGreen;
+        }
+    }
+}
