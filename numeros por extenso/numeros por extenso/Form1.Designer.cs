54d45df9fee5abc89e5b4e82be39bc85a077832f
pierryjohn01 <johnpierry18@gmail.com>
2025-06-10 13:52:38 -0300

    Add files via upload

---
+++ numeros por extenso/numeros por extenso/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace numeros_por_extenso
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            button1 = new Button();
+            label2 = new Label();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" });
+            comboBox1.Location = new Point(78, 193);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(182, 33);
+            comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(78, 165);
+            label1.Name = "label1";
+            label1.Size = new Size(186, 25);
+            label1.TabIndex = 1;
+            label1.Text = "selecione um número:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(305, 178);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 60);
+            button1.TabIndex = 2;
+            button1.Text = "Gerar extenso";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(476, 165);
+            label2.Name = "label2";
+            label2.Size = new Size(176, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Número por extenso";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(540, 376);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 4;
+            button2.Text = "Fechar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(476, 195);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(176, 31);
+            textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private Label label1;
+        private Button button1;
+        private Label label2;
+        private Button button2;
+        private TextBox textBox1;
+    }
+}
