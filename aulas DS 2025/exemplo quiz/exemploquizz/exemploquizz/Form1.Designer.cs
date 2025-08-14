eaae66c4cd0d2056fdfe42bf052fb419a1c8949c
pierryjohn01 <johnpierry18@gmail.com>
2025-08-14 14:34:21 -0300

    Add files via upload

---
+++ aulas DS 2025/exemplo quiz/exemploquizz/exemploquizz/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace exemploquizz
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(181, 384);
+            button1.Name = "button1";
+            button1.Size = new Size(105, 54);
+            button1.TabIndex = 0;
+            button1.Text = "INICIAR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(543, 384);
+            button2.Name = "button2";
+            button2.Size = new Size(105, 54);
+            button2.TabIndex = 1;
+            button2.Text = "SAIR";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "              ";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+    }
+}
